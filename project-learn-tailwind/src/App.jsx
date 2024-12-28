c6024c153a0fd1b8c043e2dae6d68e72e4a076fa
Viet Nguyen <>
2024-12-28 13:16:43 +0700

    Tailwind CSS toàn tập dành cho người mới phần 2

--- project-learn-tailwind/src/App.jsx
+++ project-learn-tailwind/src/App.jsx
@@ -1,3 +1,54 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const cards = [
+    {
+      image:
+        "https://plus.unsplash.com/premium_photo-1677343210638-5d3ce6ddbf85?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8dHJhdmVsfGVufDB8fDB8fHww",
+      title: "Enjoy the great cold",
+      subtitle: "6,679 properties",
+    },
+    {
+      image:
+        "https://plus.unsplash.com/premium_photo-1719843013722-c2f4d69db940?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8dHJhdmVsfGVufDB8fDB8fHww",
+      title: "Enjoy the great cold",
+      subtitle: "6,679 properties",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1533104816931-20fa691ff6ca?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D",
+      title: "Enjoy the great cold",
+      subtitle: "6,679 properties",
+    },
+  ];
+
+  return (
+    <div className="pt-12">
+      <h1 className="text-center text-black text-4xl font-semibold mb-5">
+        Live anywhere
+      </h1>
+
+      <h2 className="text-center text-gray-500 text-2xl font-light mb-10">
+        Keep calm & travel on
+      </h2>
+
+      <div className="grid grid-cols-3 gap-x-8 max-w-6xl mx-auto">
+        {cards.map((card) => (
+          <div key={card.image}>
+            <div className="h-96 mb-5">
+              <img
+                src={card.image}
+                alt="image"
+                className="w-full h-full object-cover rounded-lg"
+              />
+            </div>
+            <h3 className="text-center font-medium text-lg mb-3">
+              {card.title}
+            </h3>
+            <span className="block text-center text-gray-500 text-sm">
+              {card.subtitle}
+            </span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
